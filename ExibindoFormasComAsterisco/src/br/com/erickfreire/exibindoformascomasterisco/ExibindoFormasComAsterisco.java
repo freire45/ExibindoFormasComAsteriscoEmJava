b9ce8b5ba6239ecdce5f3a6c61dfe82dbb068c12
Erick Freire <erilefr@gmail.com>
2024-01-10 22:11:59 -0300

    V1
    V1

---
+++ ExibindoFormasComAsterisco/src/br/com/erickfreire/exibindoformascomasterisco/ExibindoFormasComAsterisco.java
@@ -0,0 +1,18 @@
+package br.com.erickfreire.exibindoformascomasterisco;
+
+public class ExibindoFormasComAsterisco {
+
+	public static void main(String[] args) {
+		System.out.printf("*********    ***      *        *     %n"
+				        + "*       *   *   *    ***      * *    %n"
+				        + "*       *  *     *  *****    *   *   %n"
+				        + "*       *  *     *    *     *     *  %n"
+				        + "*       *  *     *    *    *       * %n"
+				        + "*       *  *     *    *     *     *  %n"
+				        + "*       *  *     *    *      *   *   %n"
+				        + "*       *   *   *     *       * *    %n"
+				        + "*********    ***      *        *     %n");
+
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ExibindoFormasComAsteriscoEmJava
- Exibindo Formas Com Asterisco Em Java
+
+**Exibindo Formas Com Asterisco Em Java**
+
+![Exibindo Formas Com Asterisco](exibindoformascomasterisco.jpg) <br><br>
+
+Autor: Erick Leite Freire<br>
+Criado em: 10-01-2024<br>
+Site: [Erick Freire](https://www.erickfreire.com.br)<br>
